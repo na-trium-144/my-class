24f04947f6334fde46d4e83a66defa50ded1c4ba
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2022-10-04 15:17:21 +0900

    時刻反映

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, redirect, url_for, render_template, make_response
 from dotenv import dotenv_values
 import os
-import time
+import datetime
 
 config = dotenv_values()  # take environment variables from .env.
 rootdir = config["ROOTDIR"]
@@ -11,17 +11,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    day = 1
+    now = datetime.datetime.now()
+    day = now.weekday() #月=0
     cl = 0
+    for cltime in config["TIME"].split():
+        if now.time() < datetime.time(hour=int(cltime[:2]), minute=int(cltime[-2:]), second=0):
+            break
+        cl += 1
     targetdir = getdir(day, cl)
     files = []
     if targetdir:
         files = os.listdir(targetdir)
         files = [f for f in files if f.endswith(".pdf") or f.endswith(".PDF")]
-        return render_template("index.html", day="月火水木金土日"[day], cl=cl, files=files, targetdir=targetdir)
+        return render_template("index.html", day=day, cl=cl, files=files, targetdir=targetdir)
 
 def getdir(day, cl):
-    dirs = config[f"DIR{day}"].split(":")
+    dirs = ""
+    if f"DIR{day}" in config:
+        dirs = config[f"DIR{day}"].split(":")
     targetdir = ""
     if cl >= 0 and cl < len(dirs):
         targetdir = os.path.join(rootdir, dirs[cl])
