d09e7743c5962f1f0cf1aabe91b8f518e6e1d1aa
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2022-10-05 17:36:54 +0900

    bulma cssを追加

--- app.py
+++ app.py
@@ -10,20 +10,27 @@ rootdir = config["ROOTDIR"]
 app = Flask(__name__)
 
 @app.route("/")
-def root():
-    now = datetime.datetime.now()
-    day = now.weekday() #月=0
-    cl = 0
-    for cltime in config["TIME"].split():
-        if now.time() < datetime.time(hour=int(cltime[:2]), minute=int(cltime[-2:]), second=0):
-            break
-        cl += 1
-    targetdir = getdir(day, cl)
+@app.route("/<day>/<cl>")
+def root(day=None, cl=None):
+    if cl is None:
+        now = datetime.datetime.now()
+        day = now.weekday() #月=0
+        cl = 0
+        for cltime in config["TIME"].split():
+            if now.time() < datetime.time(hour=int(cltime[:2]), minute=int(cltime[-2:]), second=0):
+                break
+            cl += 1
+    else:
+        day = int(day)
+        cl = int(cl)
+    cl_length = len(config["TIME"].split())
+    cldir = getdir(day, cl)
+    targetdir = os.path.join(rootdir, cldir)
     files = []
-    if targetdir:
+    if cldir and os.path.exists(targetdir):
         files = os.listdir(targetdir)
         files = [f for f in files if f.endswith(".pdf") or f.endswith(".PDF")]
-        return render_template("index.html", day=day, cl=cl, files=files, targetdir=targetdir)
+    return render_template("index.html", day=day, cl=cl, cl_length=cl_length, files=files, cldir=cldir)
 
 def getdir(day, cl):
     dirs = ""
@@ -31,15 +38,17 @@ def getdir(day, cl):
         dirs = config[f"DIR{day}"].split(":")
     targetdir = ""
     if cl >= 0 and cl < len(dirs):
-        targetdir = os.path.join(rootdir, dirs[cl])
-    return targetdir
+        targetdir = dirs[cl]
+        return targetdir
+    return ""
 
 @app.route("/<day>/<cl>/<filename>")
 def getfile(day, cl, filename):
     day = int(day)
     cl = int(cl)
-    target = os.path.join(getdir(day, cl), filename)
-    if os.path.exists(target):
+    cldir = getdir(day, cl)
+    target = os.path.join(rootdir, cldir, filename)
+    if cldir and os.path.exists(target):
         with open(target, "rb") as f:
             res = make_response(f.read())
             res.headers["content-type"] = "application/pdf"
